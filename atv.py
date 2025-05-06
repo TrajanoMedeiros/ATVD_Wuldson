1d5ae96b049786a00960cfc9d6d00e19e36e475a
pedrotti007 <opedrotti007>
2025-05-06 20:21:10 -0300

    Boa!

---
+++ atv.py
@@ -0,0 +1,7 @@
+nome = (input("Digite seu nome:"))
+
+print(f"Olá {nome}! Seja bem vindo ao meu GitHub!")
+
+
+
+print('Caso queria acessar mais informacoes: https://github.com/pedrotti007 Obrigado!')
